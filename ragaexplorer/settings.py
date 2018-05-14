48495f8cb02744bf6da63a348df9d1a9bdfdcaf3
Rama Krishna <siddarth21@hotmail.com>
2018-05-14 23:23:03 +0800

    Updated search functionality

--- ragaexplorer/settings.py
+++ ragaexplorer/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ragas'
+    'django_filters',
+    'ragas',
 ]
 
 MIDDLEWARE = [
--- ragas/views.py
+++ ragas/views.py
@@ -1,25 +1,28 @@
 from ragas.models import Raga
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.response import Response
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 from ragas.serializers import RagaSerializer
 
+# ragas/
+# ragas/?search=char => Charukeshi
 
 class RagaList(generics.ListAPIView):
+    """
+    Return a list of all the existing ragas.
+    """
     queryset = Raga.objects.all()
     serializer_class = RagaSerializer
-    def get_queryset(self):
-        namestring = self.request.GET.get('name', None)
-        if(namestring or namestring == ''):
-            return Raga.objects.filter(name__istartswith = namestring)
-        else:
-            return Raga.objects.all()
-
-
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['^name']
+    # filter_fields = ['arohanam', 'avarohanam']
 
 
 class RagaDetail(generics.RetrieveAPIView):
+    """
+    Return the raga detail.
+    """
     serializer_class = RagaSerializer
 
     def get_queryset(self):
